ec70ddede7593b4f1a5843001d27c1c3e2b32b68
WertApk <halilmerta12@gmail.com>
2026-05-27 01:51:42 +0300

    Ates etme sistemi eklendi

--- GalagaClone/main.cpp
+++ GalagaClone/main.cpp
@@ -4,13 +4,16 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Galaga Clone");
 
-    // Oyuncu gemisi
+    // Oyuncu
     sf::RectangleShape player(sf::Vector2f(80.f, 20.f));
-
     player.setFillColor(sf::Color::Green);
-
-    // Ekranın altına yerleştir
     player.setPosition(360.f, 520.f);
+
+    // Mermi
+    sf::RectangleShape bullet(sf::Vector2f(5.f, 20.f));
+    bullet.setFillColor(sf::Color::Red);
+
+    bool bulletActive = false;
 
     while (window.isOpen())
     {
@@ -24,21 +27,48 @@ int main()
             }
         }
 
-        // Hareket
+        // Oyuncu hareketi
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         {
-            player.move(-0.3f, 0.f);
+            player.move(-0.5f, 0.f);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            player.move(0.3f, 0.f);
+            player.move(0.5f, 0.f);
+        }
+
+        // Ateş etme
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !bulletActive)
+        {
+            bullet.setPosition(
+                player.getPosition().x + 37.f,
+                player.getPosition().y
+            );
+
+            bulletActive = true;
+        }
+
+        // Mermi hareketi
+        if (bulletActive)
+        {
+            bullet.move(0.f, -0.7f);
+
+            // Ekrandan çıkınca sıfırla
+            if (bullet.getPosition().y < 0)
+            {
+                bulletActive = false;
+            }
         }
 
         window.clear();
 
-        // Oyuncuyu çiz
         window.draw(player);
+
+        if (bulletActive)
+        {
+            window.draw(bullet);
+        }
 
         window.display();
     }
